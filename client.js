8fa28df8d98d360b575016fe257957a285b3e9b1
Michael Murphy <mmurph91@charlotte.edu>
2026-06-19 12:33:39 -0400

    blue squar

--- client.js
+++ client.js
@@ -1,9 +1,31 @@
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
+let squareXoffset = 0;
+let squareYoffset = 0;
 function resizeCanvas() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    ctx.fillStyle = 'blue';
-    ctx.fillRect(50, 50, 100, 100);
 }
-window.addEventListener('resize', resizeCanvas);
+window.addEventListener('keydown', (event) => {
+    keyPressed = event.key.toLowerCase();
+    switch (keyPressed){
+        case 'w':
+            squareYoffset -= 10;
+            break;  
+        case 's':
+            squareYoffset += 10;
+            break;
+    }
+    switch (keyPressed){
+        case 'a':
+            squareXoffset -= 10;
+            break;  
+        case 'd':
+            squareXoffset += 10;
+            break;
+    }
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(canvas.width*0.25+squareXoffset, canvas.height*0.25+squareYoffset, canvas.width*0.5, canvas.height*0.5);
+    
+});
+resizeCanvas();
